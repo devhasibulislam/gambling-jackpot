f14b6c56f50f3361c18fb27b9f218430214a4067
hasibulislam999 <hasib143sl@gmail.com>
2022-07-21 20:02:46 +0600

    home route done

--- src/routes/Home/Banner.js
+++ src/routes/Home/Banner.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import slide1 from "../../assets/slider-thumbs/1.jpg";
-import slide2 from "../../assets/slider-thumbs/2.jpg";
-import slide3 from "../../assets/slider-thumbs/3.jpg";
-import slide4 from "../../assets/slider-thumbs/4.jpg";
+import slide1 from "../../assets/slider-thumbs/slide-1.jpg";
+import slide2 from "../../assets/slider-thumbs/slide-2.jpg";
+import slide3 from "../../assets/slider-thumbs/slide-3.jpg";
+import slide4 from "../../assets/slider-thumbs/slide-4.jpg";
+import slide5 from "../../assets/slider-thumbs/slide-5.jpg";
 
 const Banner = () => {
-    const slides = [slide1, slide2, slide3, slide4];
+    const slides = [slide1, slide2, slide3, slide4, slide5];
     return (
         <section className='container'>
             <Carousel>
@@ -14,15 +15,12 @@ const Banner = () => {
                     slides.map((slide, index) => <Carousel.Item
                         key={index}
                         interval={1000}
-                        touch={true}
-                    >
+                    >   
                         <img
                             className="d-block w-100"
                             src={slide}
                             alt={`slide${index + 1}`}
                             style={{
-                                height: '250px',
-                                width: '500px',
                                 objectFit: 'cover'
                             }}
                         />
