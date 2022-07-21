61cc85dccde0b3b0f035735cfd89c87d0bc770f5
hasibulislam999 <hasib143sl@gmail.com>
2022-07-21 23:25:23 +0600

    header redesign

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,40 +1,53 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 const Header = () => {
     return (
-        <Navbar bg="dark" variant='dark' expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-                {/* <Navbar.Brand href="#" title='gambling jackpot'>G.J.</Navbar.Brand> */}
+                {/* <Navbar.Brand href="#" title="gambling jackpot">G.J.</Navbar.Brand> */}
                 <OverlayTrigger
                     overlay={
-                        <Tooltip id={`tooltip-bottom`}>
+                        <Tooltip id={"tooltip-bottom"}>
                             <strong>gambling jackpot</strong>.
                         </Tooltip>
                     }
                 >
-                    <Navbar.Brand href="#" title='gambling jackpot'>gam-jack</Navbar.Brand>
+                    <Navbar.Brand href="#" title="gambling jackpot">gam-jack</Navbar.Brand>
                 </OverlayTrigger>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="ms-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
+                        style={{ maxHeight: "100px" }}
                         navbarScroll
                     >
                         <Nav.Link href="#action1">Home</Nav.Link>
                         <Nav.Link href="#action2">Hasibul Islam $(59.77)</Nav.Link>
-                        <Nav.Link href="#action3">Statement</Nav.Link>
+                        <NavDropdown title="Statements" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Deposits</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">
+                                Withdrawal
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action5">
+                                Transactions
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action6">
+                                Table view
+                            </NavDropdown.Item>
+                        </NavDropdown>
                         <Nav.Link href="#action4">Logout</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     );
-}
+};
 
 export default Header;
