cb3f9e6b929f0c7dd33b431b87a2d570f810259f
hasibulislam999 <hasib143sl@gmail.com>
2022-07-21 18:44:44 +0600

    header and footer done

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <section className='bg-dark text-white py-2'>
-            <p className='py-0 my-0'>copyright © {new Date().getFullYear()} <a href="http://gambling-jackpot.vercel.app/" className='py-0 my-0'>gam-jack</a></p>
+            <p className='py-0 my-0'>copyright © {new Date().getFullYear()} <a
+                href="http://gambling-jackpot.vercel.app/"
+                className='py-0 my-0'
+                target={'_blank'}
+            >gam-jack</a></p>
         </section>
     );
 }
